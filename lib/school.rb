b7007e24d96f4d4ca0cb7b0f92df42ac3871a4e6
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-04-01 18:28:14 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,13 +2,16 @@
 class School
 
   attr_reader :name
-  @roster = {}
+  roster = {}
 
   def initialize(name)
     @name = name
     @roster = roster 
   end
 
+  def roster
+    @roster = roster
+    
   def add_student(name, year)
     @roster[year] = ["#{name}"]
   end
